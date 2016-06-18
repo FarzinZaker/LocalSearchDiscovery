01a23f120febd74ca7f6bbd5fae34c24ffb806bb
roohi <srmo642002@gmail.com>
2016-06-18 08:46:29 +0430

    init

--- grails-app/controllers/agahisaz/PlaceController.groovy
+++ grails-app/controllers/agahisaz/PlaceController.groovy
@@ -46,7 +46,67 @@ class PlaceController {
     }
     static def cur = System.currentTimeMillis()
     static def count = 0
+    def searchImportJSON() {
+        def sort = [:]
+        def query = [:]
+        def aggregateQuery
+        def tags
+        if (params.tags)
+            tags = params.tags?.split('[|]')?.toList() ?: []
+        def projection = null
+        if (params.province && params.city)
+            query << [province: params.province]
+        if (params.city)
+            query << [city: params.city]
 
+        def queryString = params.id?.toString()?.trim() ?: ''
+        request.setAttribute("query", queryString)
+        def category = Category.findByName(queryString)
+        if (category) {
+            request.setAttribute("queryCategory", category?.id)
+            queryString = ''
+            query << [category: [$in: CategoryCache.findCategory(category.id)?.childIdList ?: [] + [category.id]]]
+        }
+        if (tags)
+            query << [tags: [$all: tags]]
+
+        if (queryString != '') {
+            request.setAttribute("queryCategory", "0")
+            //search using geoWithin
+            query << [$text: [$search: params.id]]
+            projection = [score: [$meta: "textScore"]]
+            sort << ['score': ['$meta': 'textScore']]
+            if (params.near) {
+                def nearParams = params.near?.toString()?.split(',')
+                if (nearParams.size() == 2) {
+                    def latitude = nearParams[0]?.toDouble()
+                    def longitude = nearParams[1]?.toDouble()
+                    query << [location: [$geoWithin: [$center: [[latitude, longitude], (params.radius?.toDouble()) ?: 0.03D]]]]
+                }
+            }
+            aggregateQuery = query.clone()
+        } else {
+            //search using geoNear
+            aggregateQuery = query.clone()
+            if (params.near) {
+                def nearParams = params.near?.toString()?.split(',')
+                if (nearParams.size() == 2) {
+                    def latitude = nearParams[0]?.toDouble()
+                    def longitude = nearParams[1]?.toDouble()
+                    query << [location: [
+                            $near       : [latitude, longitude],
+                            $maxDistance: 0.03D
+                    ]
+                    ]
+                    aggregateQuery << [location: [$geoWithin: [$center: [[latitude, longitude], (params.radius?.toDouble()) ?: 0.03D]]]]
+                }
+            }
+        }
+
+        def places = mongoService.getCollection("place").find(query, projection)?.sort(sort)?.limit(200)?.findAll()?.each { place -> place.category = CategoryCache.findCategory(place.category) } ?: []
+
+        render(places as JSON)
+    }
     def importJson() {
         if (count < 0)
             count = 0
