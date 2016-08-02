4d1f183cc431a17df61abad823dbafe51731f4b2
Farzin <Farzin@SiliconValley>
2016-08-02 16:59:44 +0430

    info pages

--- grails-app/jobs/agahisaz/SitemapJob.groovy
+++ grails-app/jobs/agahisaz/SitemapJob.groovy
@@ -5,6 +5,7 @@ class SitemapJob {
 
     static startDelay = 60000
     static timeout = 12 * 60 * 60 * 1000l
+    static cronExpression = '0 0 1 1/1 * ? *'
     static concurrent = false
 
     def siteMapService
